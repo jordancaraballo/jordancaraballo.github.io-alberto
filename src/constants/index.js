6589562dcb2af839138e512ed1471e66f23f70f0
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2025-11-19 19:33:41 -0500

    Adding deployment

--- src/constants/index.js
+++ src/constants/index.js
@@ -1149,7 +1149,7 @@ export const aboutMe = {
   tagLine:
     "MTS @ Oracle | 10x Hackathon Winner 🏆 | ETHIndia'22,24 Winner | NITK'24 | PBA-5",
   intro:
-    "Software Developer from India who is either busy improving his craft or pondering over the next big idea.",
+    "WOWOWOOWOWOWOOWOWOWOWOWo",
 };
 
 // The maximum number of PRs to be displayed in the Open Source Contributions section.
